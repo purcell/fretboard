2bbd750fd24afbd77ed243186f034bfd5ca20bdb
Steve Purcell <steve@sanityinc.com>
2019-12-31 11:35:02 +1300

    Give scales names

--- src/Scale.elm
+++ src/Scale.elm
@@ -8,49 +8,55 @@ module Scale exposing
     , minor
     , minorPentatonic
     , minorTriad
+    , scaleName
     )
 
 
 type Scale
     = -- A list of semitone degrees within a scale
-      Scale (List Int)
+      Scale { name : String, degrees : List Int }
+
+
+scaleName : Scale -> String
+scaleName (Scale s) =
+    s.name
 
 
 isSemitoneDegreeInScale : Scale -> Int -> Bool
-isSemitoneDegreeInScale (Scale degrees) degree =
-    List.member degree degrees
+isSemitoneDegreeInScale (Scale s) degree =
+    List.member degree s.degrees
 
 
 chromatic : Scale
 chromatic =
-    Scale [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ]
+    Scale { name = "Chromatic", degrees = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ] }
 
 
 major : Scale
 major =
-    Scale [ 0, 2, 4, 5, 7, 9, 11 ]
+    Scale { name = "Major", degrees = [ 0, 2, 4, 5, 7, 9, 11 ] }
 
 
 minor : Scale
 minor =
-    Scale [ 0, 2, 3, 5, 7, 8, 10 ]
+    Scale { name = "Minor", degrees = [ 0, 2, 3, 5, 7, 8, 10 ] }
 
 
 majorTriad : Scale
 majorTriad =
-    Scale [ 0, 4, 7 ]
+    Scale { name = "Major triad", degrees = [ 0, 4, 7 ] }
 
 
 minorTriad : Scale
 minorTriad =
-    Scale [ 0, 3, 7 ]
+    Scale { name = "Minor triad", degrees = [ 0, 3, 7 ] }
 
 
 harmonicMinor : Scale
 harmonicMinor =
-    Scale [ 0, 2, 3, 5, 7, 8, 11 ]
+    Scale { name = "Harmonic Minor", degrees = [ 0, 2, 3, 5, 7, 8, 11 ] }
 
 
 minorPentatonic : Scale
 minorPentatonic =
-    Scale [ 0, 3, 5, 8, 10 ]
+    Scale { name = "Minor Pentatonic", degrees = [ 0, 3, 5, 8, 10 ] }
